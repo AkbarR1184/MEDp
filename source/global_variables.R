b590d41f03013cd5f3fead5b060ff11d19ee519c
AkbarR1184 <rahmati_ziveh@fzp.czu.cz>
2024-10-07 15:18:09 +0000

    -Minor changes in global_variable.R

--- source/global_variables.R
+++ source/global_variables.R
@@ -7,15 +7,43 @@ PATH_GEO_DATA <- 'data/geo_data/'
 PATH_TIDY_DATA <- 'data/tidy/'
 
 ## Datasets
-PREC_GLOBAL_DATASETS <- c("e-obs", "em-earth","era5-land", "gpcc",  
-                          "gpcp", "gpm-imerg", "gsmap","merra2-land","mswep",  
-                          "mswx")
-
+PREC_GLOBAL_DATASETS <- c(
+  "e-obs",
+  "em-earth",
+  "era5-land",
+  "gpcc",
+  "gpcp",
+  "gpm-imerg",
+  "gsmap",
+  "merra2-land",
+  "mswep",
+  "mswx"
+)
+PREC_UPDATED_NAMES <- c(
+  "e-obs" = "E-OBS v29e",
+  "em-earth" = "EM-EARTH",
+  "era5-land" = "ERA5-Land",
+  "gpcc-v2020" = "GPCC v2020",
+  "gpcp-v3-2" = "GPCP v3.2",
+  "gpm-imerg-v6" = "GPM-IMERG v6",
+  "gsmap-v8" = "GSMaP v8",
+  "merra2-land" = "MERRA2-Land",
+  "mswep-v2-8" = "MSWEP v2.8",
+  "mswx-past" = "MSWX-Past"
+)
 # Types
-PREC_DATASETS_OBS <- c("e-obs", "em-earth", "gpcc")
-PREC_DATASETS_REANAL <- c("era5-land", "merra2-land", "mswep", "mswx")
-PREC_DATASETS_REMOTE <- c("gpcp", "gpm-imerg", "gsmap")
-
+PREC_SOURCE <- list(
+  "e-obs" = "gauge-based",
+  "em-earth" = "gauge-based",
+  "era5-land" = "reanalysis-based",
+  "gpcc" = "gauge-based",
+  "gpcp" = "satellite-based",
+  "gpm-imerg" = "satellite-based",
+  "gsmap" = "satellite-based",
+  "merra2-land" = "reanalysis-based",
+  "mswep" = "reanalysis-based",
+  "mswx" = "reanalysis-based"
+)
 ## Constants
 # Time
 PERIOD_START <- as.Date("2001-01-01")
